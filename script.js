cfdfd1c74bea5f017ec439a63181e67a82ec9191
MertKy <mertky24@gmail.com>
2024-09-04 00:25:57 +0300

    updatedv2

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-document.getElementById('clickButton').addEventListener('click', function() {
-    document.getElementById('message').textContent = 'Butona tıkladınız!';
+// Ana Sayfadaki butona tıklama olayı
+document.addEventListener('DOMContentLoaded', () => {
+    const alertButton = document.getElementById('alertButton');
+
+    if (alertButton) {
+        alertButton.addEventListener('click', () => {
+            alert('Butona tıkladınız!');
+        });
+    }
+
+    // İletişim formunu kontrol etme
+    const contactForm = document.getElementById('contactForm');
+
+    if (contactForm) {
+        contactForm.addEventListener('submit', (event) => {
+            event.preventDefault(); // Formun sayfayı yeniden yüklemesini engelle
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const message = document.getElementById('message').value;
+
+            alert(`Adınız: ${name}\nEmail: ${email}\nMesajınız: ${message}`);
+        });
+    }
 });
